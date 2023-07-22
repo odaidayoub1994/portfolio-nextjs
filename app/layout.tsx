9e4ec0ba75f222787285e540de34dac05d91eb0a
odaidayoub1994 <118967390+odaidayoub1994@users.noreply.github.com>
2023-07-23 01:13:48 +0400

    update app title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import { ReactNode } from 'react';
 
 export const metadata: Metadata = {
-  title: 'Portfolio | Odai Dayoub',
+  title: 'Odai Dayoub | Software Engineer',
   description: '',
 };
 
