6d5c44e3488e99f7078637698cf3e88691aeced9
odaidayoub1994 <118967390+odaidayoub1994@users.noreply.github.com>
2023-07-22 23:02:58 +0400

    add ids to the components

--- components/main/Main.tsx
+++ components/main/Main.tsx
@@ -6,7 +6,7 @@ import { BsFillPersonLinesFill } from 'react-icons/bs';
 
 const Main = () => {
   return (
-    <div className="w-fill h-screen text-center">
+    <div id="home" className="w-fill h-screen text-center">
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
         <div>
           <p className="uppercase text-small tracking-widest text-gray-600">
